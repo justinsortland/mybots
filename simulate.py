fefd96708d7abbe8cf93f61e1bb88a2f0463ddb9
justinsortland <justinsortland2025@u.northwestern.edu>
2023-01-08 21:10:46 -0600

    Starting work on simulation.

--- simulate.py
+++ simulate.py
@@ -3,9 +3,10 @@ import pybullet_data
 import time
 
 physicsClient = p.connect(p.GUI)
-#p.setGravity(0,0,-9.8)
+p.setGravity(0,0,-9.8)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 planeId = p.loadURDF("plane.urdf")
+p.loadSDF("boxes.sdf")
 for i in range(1000):
     p.stepSimulation()
     time.sleep(1/60)
