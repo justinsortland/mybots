bb4f7b4c63a84e7f2847440b2b48a55d000ec9a3
justinsortland <justinsortland2025@u.northwestern.edu>
2023-01-12 17:21:20 -0600

    Starting work on motors.

--- simulate.py
+++ simulate.py
@@ -7,15 +7,25 @@ import numpy
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 planeId = p.loadURDF("plane.urdf")
+p.setGravity(0,0,-9.8)
 p.loadSDF("world.sdf")
 robotId = p.loadURDF("body.urdf")
 pyrosim.Prepare_To_Simulate(robotId)
-backLegSensorValues = numpy.zeros(10000)
 
-for i in range(10000):
+backLegSensorValues = numpy.zeros(1000)
+frontLegSensorValues = numpy.zeros(1000)
+
+
+for i in range(1000):
     p.stepSimulation()
+
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+
+    pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName="Torso_BackLeg", controlMode=p.POSITION_CONTROL, targetPosition=0.0, maxForce=500)
+
     time.sleep(1/60)
 
 numpy.save("data/backLegSensorValues.npy", backLegSensorValues)
+numpy.save("data/frontLegSensorValues.npy", frontLegSensorValues)
 p.disconnect()
